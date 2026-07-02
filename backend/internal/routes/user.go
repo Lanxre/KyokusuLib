adcf26ac272933e4410dd45472066b2bc83cd143
Lanxre <tat4vit7@gmail.com>
2026-07-02 13:08:12 +0300

    feat(backend): add route for update status

--- backend/internal/routes/user.go
+++ backend/internal/routes/user.go
@@ -13,4 +13,6 @@ func (a *UserRoutes) Register(cfg *config.Config, r *mux.Router) {
 	s.HandleFunc("/{id:[0-9]+}", a.Handler.GetUserById).Methods("GET")
 	s.HandleFunc("/activity", middleware.AuthMiddleware(a.Handler.UpdateUserStatus, cfg.JWTSecret)).Methods("POST")
 	s.HandleFunc("/tag", middleware.AuthMiddleware(a.Handler.UpdateUserTag, cfg.JWTSecret)).Methods("PUT")
+
+	s.HandleFunc("/status", middleware.AuthMiddleware(middleware.RoleGuard(a.Handler.UpdateUserStatus, middleware.RoleModerator), cfg.JWTSecret)).Methods("PUT") 
 }
